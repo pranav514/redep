ff38cbabce4c0d7e639b30aefd861391fd3413be
pranav514 <pranavsalunkhe327@gmail.com>
2024-02-22 23:22:33 +0530

    upload ,deploy service

---
+++ redep upload service/src/aws.ts
@@ -0,0 +1,19 @@
+import {S3} from "aws-sdk";
+import fs from "fs";
+
+const s3 = new S3({
+    accessKeyId: "",
+    secretAccessKey:"",
+    endpoint:""
+})
+
+
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+    const fileContent = fs.readFileSync(localFilePath);
+    const response = await s3.upload({
+        Body: fileContent,
+        Bucket: "deployment",
+        Key: fileName,
+    }).promise();
+    console.log(response);
+}
